e19d42170f6b508e9b937ac4416625f6c902d2a1
Chalitha Perera <chalithaudara@gmail.com>
2014-05-17 11:34:45 +0530

    initail commit with genericrdf default configuration settings

---
+++ src/main/java/org/apache/stanbol/org/apache/stanbol/entityhub/indexing/yago/App.java
@@ -0,0 +1,13 @@
+package org.apache.stanbol.org.apache.stanbol.entityhub.indexing.yago;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
